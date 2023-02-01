61054d363013b52083a6ea774eaba2a9a485e939
Marco Uriel Vargas Torres <impresion.clientes@gmail.com>
2023-02-01 09:38:12 -0600

    change for cicle in reorganize data helper

--- horario/src/Helpers/helpers.js
+++ horario/src/Helpers/helpers.js
@@ -2,7 +2,7 @@
 const reorganizar = data => {
     const array = [];
     const d = data;
-    for(let i = 0; i < 363; i++){
+    for(let i = 0; i < d.Grupo.length; i++){
         const fila = { "Grupo": d.Grupo[i], "Asignatura" : d.Asignatura[i], "Profesor" : d.Profesor[i], "Edificio" : d.Edificio[i], "Salon" : d.Salon[i], "L" : d.Lun[i], "M" : d.Mar[i], "X": d.Mie[i], "J": d.Jue[i], "V" : d.Vie[i] }
         array.push(fila);
     }
