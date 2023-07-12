f39fb6df58fa0e3896a4663ce42ff485a52d0726
Marco Uriel Vargas Torres <impresion.clientes@gmail.com>
2023-07-11 22:33:13 -0600

    Ligeros cambios

--- python/converting.py
+++ python/converting.py
@@ -1,3 +1,3 @@
 import pandas as pd
-df = pd.read_csv (r'C:\Users\SibylSystem\Desktop\Code\Schedule\python\Horarios.csv')
-df.to_json (r'C:\Users\SibylSystem\Desktop\Code\Schedule\python\Horario.json')
+df = pd.read_csv (r'C:\Users\chopa\desktop\code\schedule\schedule-maker\python\Horarios.csv')
+df.to_json (r'C:\Users\chopa\desktop\code\schedule\schedule-maker\python\Horario.json')
